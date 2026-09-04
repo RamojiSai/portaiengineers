c5e3bf440c876383f0687de9a376d13500715c24
Benda Manikanta <bendamanikanta@Bendas-MacBook-Air.local>
2026-09-04 20:50:36 +0530

    Updates

---
+++ _next/static/chunks/app/contact/page-b29e770040f3e13f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{9306:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.bind(n,6070))}},e=>{e.O(0,[619,70,441,255,358],()=>e(e.s=9306)),_N_E=e.O()}]);
